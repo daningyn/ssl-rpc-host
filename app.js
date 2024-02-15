0d279f53964e7ffdd4d6840c0f33b2b559df78c1
danh nguyen <danh.nghuu@gmail.com>
2024-02-15 14:13:12 +0700

    update script to show landing page

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
   res.send('by daningyn');
+  res.sendFile(`${__dirname}/landing-html/landing.html`);
 })
 
 app.listen(3000)
